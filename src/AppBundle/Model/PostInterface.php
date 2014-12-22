3958bd910d80a6892b2f4b320d8d4af2a553a183
soyel <contact@soyel.fr>
2014-12-23 00:54:59 +0100

    Generate Post entity, interface and form. Rename the controller

---
+++ src/AppBundle/Model/PostInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Model;
+
+Interface PostInterface
+{
+    /**
+    * Set title
+    *
+    * @param string $title
+    * @return PostInterface
+    */
+    public function setTitle($title);
+
+    /**
+    * Get title
+    *
+    * @return string
+    */
+    public function getTitle();
+
+    /**
+    * Set content
+    *
+    * @param string $content
+    * @return PostInterface
+    */
+    public function setContent($content);
+
+    /**
+    * Get content
+    *
+    * @return string
+    */
+    public function getContent();
+}
