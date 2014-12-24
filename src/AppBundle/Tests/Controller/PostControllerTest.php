78f7b79a764ad49d60c3b4d860aaf096a63e71ef
soyel <contact@soyel.fr>
2014-12-24 18:58:19 +0100

    Add test for POST action

--- src/AppBundle/Tests/Controller/PostControllerTest.php
+++ src/AppBundle/Tests/Controller/PostControllerTest.php
@@ -8,15 +8,20 @@ use AppBundle\Tests\Fixtures\Entity\LoadPostData;
 
 class PostControllerTest extends WebTestCase
 {
+    /**
+     * @var Symfony\Bundle\FrameworkBundle\Client $client
+     */
+    protected $client;
+
     public function testGet()
     {
-        $client = static::createClient();
+        $this->client = static::createClient();
         $fixtures = array('AppBundle\Tests\Fixtures\Entity\LoadPostData');
         $this->loadFixtures($fixtures);
         $post = array_pop(LoadPostData::$posts);
         $route =  $this->getUrl('api_v1_get_post', array('post' => $post->getId(), '_format' => 'json'));
         $client->request('GET', $route);
-        $response = $client->getResponse();
+        $response = $this->client->getResponse();
         $this->assertJsonResponse($response, 200);
         $content = $response->getContent();
         $decoded = json_decode($content, true);
@@ -25,15 +30,29 @@ class PostControllerTest extends WebTestCase
         $this->assertTrue(isset($decoded['content']));
     }
 
+    public function testJsonPost()
+    {
+        $this->client = static::createClient();
+        $this->client->request(
+            'POST',
+            '/v1/posts.json',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            '{"title":"title1","content":"content1"}'
+        );
+        $this->assertJsonResponse($this->client->getResponse(), 201, false);
+    }
+
     protected function assertJsonResponse($response, $statusCode = 200)
     {
         $this->assertEquals(
-        $statusCode, $response->getStatusCode(),
-        $response->getContent()
-    );
-    $this->assertTrue(
-    $response->headers->contains('Content-Type', 'application/json'),
-    $response->headers
-);
-}
+            $statusCode, $response->getStatusCode(),
+            $response->getContent()
+        );
+        $this->assertTrue(
+            $response->headers->contains('Content-Type', 'application/json'),
+            $response->headers
+        );
+    }
 }
